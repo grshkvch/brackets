a484854f0f036a75d3a662bb3e6dc6fb74947b4e
grshkvch <volody_200115@mail.ru>
2022-02-25 02:29:26 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length === 0) return true;
+  if (str.length % 2 === 1) return false;
+
+
+  const configObj = {}
+  for (let arr of bracketsConfig) {
+    configObj[(arr[0])] = (arr[1])
+}
+ 
+  let bracketsDeck = []
+
+  for (let bracket of str) {
+    let topDeck = bracketsDeck[bracketsDeck.length - 1];
+    if (bracket === configObj[topDeck]) {
+      bracketsDeck.pop();
+    } else {
+      bracketsDeck.push(bracket)
+    }
+  }
+
+  return bracketsDeck.length === 0
 }
